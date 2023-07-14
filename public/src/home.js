bd319594546e3c556fc5a79e337b7ee9328b6a3f
TYinNY <125169961+TYinNY@users.noreply.github.com>
2023-07-14 15:54:51 -0400

    Add files via upload

--- public/src/home.js
+++ public/src/home.js
@@ -1,36 +1,37 @@
+function limitArrayLength(array, limit) {
+  return array.slice(0, limit);
+}
+
 function getTotalBooksCount(books) {
-  let bookCount = books.map((book) => book.id)
-  return bookCount.length
+  return books.length;
 }
 
 function getTotalAccountsCount(accounts) {
- let accCount = accounts.length > 0 ? accounts.map((account) => account.id) : [];
-  return accCount.length
+  return accounts.length;
 }
 
 function getBooksBorrowedCount(books) {
-  let borrowedstat = books.filter((book) => book.borrows[0].returned === false)
-  return borrowedstat.length
+  const borrowedBooks = books.filter((book) => book.borrows[0].returned === false);
+  return borrowedBooks.length;
 }
 
 function getMostCommonGenres(books) {
   let genreCount = {};
   //empty list to write ur tally counter
-
   for (let i = 0; i < books.length; i++) {
   //iterates through the books
     const genre = books[i].genre;
     //sets genre variable to the iterated book genre
     if (genre in genreCount) {
-    //creates a condition for the genreCount that looks at the genre (books[i].genre)
-      genreCount[genre]++;
-      //if true, the genre exists, the genre tallies up by 1
+     //creates a condition for the genreCount that looks at the genre (books[i].genre)
+      genreCount[genre]++; 
+       //if true, the genre exists, the genre tallies up by 1
     } else {
       genreCount[genre] = 1;
       //if false the genre object value gets added to the list and sets the tally to 1
     }
   }
- //console.log(genreCount)
+
   const sortedGenres = Object.keys(genreCount).sort((a, b) => {
     return genreCount[b] - genreCount[a];
   });
@@ -39,51 +40,46 @@ function getMostCommonGenres(books) {
     return { name: genre, count: genreCount[genre] };
   });
   //limits result to 5 with .slice method and returning new formatted object
-
   return topGenres;
+  //returns 
 }
-
 
 function getMostPopularBooks(books) {
   let borrowCount = [];
   for (let i = 0; i < books.length; i++) {
     const borrowed = books[i].borrows.length;
-    const name = books[i].title; // Change "title" to "name"
+    const name = books[i].title;
     borrowCount.push({ name, count: borrowed });
   }
   let sortedBooks = borrowCount.sort((bookA, bookB) =>
     bookA.count < bookB.count ? 1 : -1
   );
-  let topFiveBooks = sortedBooks.slice(0, 5)
-  return topFiveBooks
+  let topFiveBooks = limitArrayLength(sortedBooks, 5); //helper function
+  return topFiveBooks;
 }
 
 function getMostPopularAuthors(books, authors) {
   let authorCount = {};
-
   for (let i = 0; i < books.length; i++) {
     const book = books[i];
     const author = authors.find((author) => author.id === book.authorId);
     if (author) {
-      if (authorCount[author.name.first + ' ' + author.name.last]) {
-        authorCount[author.name.first + ' ' + author.name.last] += book.borrows.length;
+      const authorName = `${author.name.first} ${author.name.last}`;
+      if (authorCount[authorName]) {
+        authorCount[authorName] += book.borrows.length;
       } else {
-        authorCount[author.name.first + ' ' + author.name.last] = book.borrows.length;
+        authorCount[authorName] = book.borrows.length;
       }
     }
   }
-
   let sortedAuthors = [];
-
   for (let author in authorCount) {
     sortedAuthors.push({ name: author, count: authorCount[author] });
   }
-
   sortedAuthors.sort((a, b) => b.count - a.count);
-
-  return sortedAuthors.slice(0, 5);
+  let topFiveAuthors = limitArrayLength(sortedAuthors, 5); //helper function
+  return topFiveAuthors;
 }
-
 
 module.exports = {
   getTotalBooksCount,
